a02854e8220345187c0c60cb260af8b7d800ee5a
43947679890 <43947679890@ECE337N25SN790>
2026-09-11 11:00:57 -0300

    Herança

---
+++ Funcionario.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modulo2
+{
+    public class Funcionario
+    {
+        public string Nome { get; set; }
+        public double Salario { get; set; }
+        public virtual double CalcularBonusAnual()
+        {
+            return Salario * 0.10;
+        }
+    }
+    public class Gerente : Funcionario
+    {
+        public new double CalcularBonusAnual()
+        {
+            return (Salario * 0.15) + 1000.0;
+        }
+    }
+    public class Desenvolvedor : Funcionario
+    {
+        public new double CalcularBonusAnual()
+        {
+            return Salario * 0.12;
+        }
+    }
+}
